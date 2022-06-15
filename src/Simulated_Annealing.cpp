1b38043b9979ed7e6444534cffdaf424a5d5e748
x28150867 <b09901089@g.ntu.edu.tw>
2022-06-15 12:49:46 +0800

    all input

--- src/Simulated_Annealing.cpp
+++ src/Simulated_Annealing.cpp
@@ -22,9 +22,15 @@ Pin::Pin(int name, int posX, int posY, int net) {
     posY = posY;
     net = net;
 }
+class Terminal {
+   public:
+    static int width, height, spacing;
+    int posX, posY;
+};
 class Inst {
    public:
-    int posX, posY, sizeY, sizeX, pinNum;
+    int posX, posY, sizeY, sizeX, pinNum, type;
+    vector<Pin> pins;
     Inst(){};
     Inst(int posX, int posY, int sizeY, int sizeX, int pinNum);
 };
@@ -94,30 +100,38 @@ LibCell::LibCell(int name, int tech, int sizeX, int sizeY, int pinNum) {
     sizeY = sizeY;
 }
 class Die {
-    int sizeY, length, rowsizeY, tech;
+   public:
+    int lowerLeftX, lowerLeftY, higherRightX, higherRightY, gridWidth, gridHeight, tech, maxUtil, rowNum, colNum;
     vector<vector<int>> grid;
-    Die(int sizeY, int length, int rowsizeY, int tech);
+    Die(int lowerLeftX, int lowerLeftY, int higherRightX, int higherRightY);
 };
-Die::Die(int sizeY, int length, int rowsizeY, int tech) {
-    sizeY = sizeY;
-    length = length;
-    rowsizeY = rowsizeY;
-    tech = tech;
+Die::Die(int lowerLeftX, int lowerLeftY, int higherRightX, int higherRightY) {
+    lowerLeftX = lowerLeftX;
+    lowerLeftY = lowerLeftY;
+    higherRightX = higherRightX;
+    higherRightY = higherRightY;
 }
 // int HPWL() {
 // }
+void InstDeepCopy(Inst& i, LibCell& l) {
+    i.pinNum = l.getpinNum();
+    i.sizeX = l.getsizeX();
+    i.sizeY = l.getsizeY();
+}
 int main(int argc, char* argv[]) {
-    int techNum;
+    int techNum, instanceNum, netNum;
     fstream fin(argv[1]);
     fstream fout;
     fout.open(argv[2], ios::out);
     char JUNK[100], junk;
-
+    int lowerLeftX, lowerLeftY, higherRightX, higherRightY;
     fin >> JUNK;
     fin >> techNum;
     vector<int> techDieNum(techNum);
     vector<vector<LibCell>> Lib(techNum);
-
+    vector<Inst> instances;
+    vector<int> netPinNum;
+    int64_t avgWidth[2] = {0};
     // input Technology Library
 
     for (int i = 0; i < techNum; i++) {
@@ -153,7 +167,6 @@ int main(int argc, char* argv[]) {
             }
             Lib[i][j].pins = pins;
         }
-
         // cout << junk << "tech:" << tech;
     }
     // test(pass)
@@ -170,5 +183,45 @@ int main(int argc, char* argv[]) {
 
     // Todo1: constraint input
 
+    fin >> JUNK >> lowerLeftX >> lowerLeftY >> higherRightX >> higherRightY;
+    Die* dies[2];
+    dies[0] = new Die(lowerLeftX, lowerLeftY, higherRightX, higherRightY);
+    dies[1] = new Die(lowerLeftX, lowerLeftY, higherRightX, higherRightY);
+    fin >> JUNK >> dies[0]->maxUtil >> JUNK >> dies[1]->maxUtil;
+    fin >> JUNK >> JUNK >> JUNK >> dies[0]->gridHeight >> dies[0]->rowNum;
+    fin >> JUNK >> JUNK >> JUNK >> dies[1]->gridHeight >> dies[1]->rowNum;
+    fin >> JUNK >> junk >> junk;
+    dies[0]->tech = (int)junk - 65;
+    fin >> JUNK >> junk >> junk;
+    dies[1]->tech = (int)junk - 65;
+    fin >> JUNK >> Terminal::width >> Terminal::height >> JUNK >> Terminal::spacing;
+
     // Todo2: Instance input
+    fin >> JUNK >> instanceNum;
+    instances.resize(instanceNum);
+    for (int i = 0; i < instanceNum; i++) {
+        int type;
+        fin >> JUNK >> JUNK >> junk >> junk >> type;
+        instances[i].type = type - 1;
+        instances[i].pinNum = Lib[0][type].getpinNum();
+        instances[i].pins.resize(instances[i].pinNum);
+        avgWidth[0] += Lib[0][type - 1].getsizeX();
+        avgWidth[1] += Lib[1][type - 1].getsizeX();
+    }
+    avgWidth[0] /= instanceNum;
+    avgWidth[1] /= instanceNum;
+    dies[0]->gridWidth = avgWidth[0];
+    dies[1]->gridWidth = avgWidth[1];
+    // Nets Input
+    fin >>
+        JUNK >> netNum;
+    netPinNum.resize(netNum);
+    for (int i = 0; i < netNum; i++) {
+        fin >> JUNK >> JUNK >> netPinNum[i];
+        for (int j = 0; j < netPinNum[i]; j++) {
+            int targetInst, targetPin;
+            fin >> JUNK >> junk >> targetInst >> junk >> junk >> targetPin;
+            instances[targetInst - 1].pins[targetPin - 1].net = i;
+        }
+    }
 }
